a4b6283fa68ce3d65436ad48402a2e4482c9dc0d
Mal-1702 <Malhaarphadtare17@gmail.com>
2026-01-10 00:02:28 +0530

    test

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.api.v1.router import router as v1_router
 
 # Create FastAPI app
 app = FastAPI(
-    title="AI ATS Backend",
+    title="Enterprise AI backend",
     description="Enterprise ATS - POC Backend",
     version="1.0.0"
 )
+app.include_router(v1_router, prefix="/api/v1")
 
 # CORS (important for frontend later)
 app.add_middleware(
