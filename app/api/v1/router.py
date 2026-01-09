0e90761c10ab157b6d44e2b7e0a48f014fc884ed
Mal-1702 <Malhaarphadtare17@gmail.com>
2026-01-09 23:58:47 +0530

    router check file

---
+++ app/api/v1/router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.api.v1.routes import health, upload, ranking
+
+router = APIRouter()
+
+router.include_router(health.router)
+router.include_router(upload.router)
+router.include_router(ranking.router)
