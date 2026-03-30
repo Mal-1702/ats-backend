3c0dcf71b9015e2c06c21a98a959462acecbc838
Mal-1702 <Malhaarphadtare17@gmail.com>
2026-03-30 23:57:12 +0530

    small change

--- app/main.py
+++ app/main.py
@@ -27,8 +27,7 @@ allowed_origins = [
 ]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=allowed_origins,
+    CORSMiddleware,    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
